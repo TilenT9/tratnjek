35386c232cde551d2706c41e98b9c019c65bef50
Tilen <tratnjekt9@gmail.com>
2020-06-26 11:27:41 +0200

    Controller, Migration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,20 +18,11 @@
     @endif
 
     <div class="content">
+        <img src="/img/web_developer.svg" alt="">
         <div class="title m-b-md">
-            Laravel
+            Tratnjek
         </div>
 
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
     </div>
 </div>
     
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', 'ServiceController@index');
+
+Route::get('/about', function () {
+    return view('about');
 });
